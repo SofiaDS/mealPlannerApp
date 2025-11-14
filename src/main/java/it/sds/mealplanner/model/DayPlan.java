83e26bca079a07f9f2875980d0815f869d4aff79
sofia_ds <s.dellaspora@outlook.com>
2025-11-14 14:58:25 +0100

    fixed structure and new recipe assignement in DayPlan
    Signed-off-by: sofia_ds <s.dellaspora@outlook.com>

--- src/main/java/it/sds/mealplanner/model/DayPlan.java
+++ src/main/java/it/sds/mealplanner/model/DayPlan.java
@@ -2,6 +2,7 @@ package it.sds.mealplanner.model;
 
 import java.time.DayOfWeek;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class DayPlan {
@@ -15,26 +16,52 @@ public class DayPlan {
         }
         this.day = day;
         this.meals = new ArrayList<>();
+
+        /*
+        always create a fixed structure of mealSlot in a day
+         */
+        meals.add(new MealSlot(day, MealType.BREAKFAST, null));
+        meals.add(new MealSlot(day, MealType.SNACK, null));
+        meals.add(new MealSlot(day, MealType.LUNCH,null));
+        meals.add(new MealSlot(day, MealType.SNACK,null));
+        meals.add(new MealSlot(day, MealType.DINNER,null));
     }
 
     public DayOfWeek getDay() {
         return day;
     }
-
-    public List<MealSlot> getMeals() {
-        return meals;
-    }
-
-    public void addMeal(MealSlot mealSlot) {
-        if (mealSlot == null) {
-            throw new IllegalArgumentException("MealSlot cannot be null");
-        }
-        // TBD check that in a day there is no double lunch/dinner/breakfast
-        meals.add(mealSlot);
-    }
     /*
      * TBD when returning the day plan I want a fixed order breakfast - snack - lunch - snack - dinner
      * */
+    public List<MealSlot> getMeals() {
+        return Collections.unmodifiableList(meals);
+    }
+    public void assignRecipe(MealType type, Recipe recipe){
+        if (type == null) {
+            throw new IllegalArgumentException("Meal type cannot be null!");
+        }
+
+        if (type != MealType.SNACK){
+            //case where a MealType can only be present one
+            for (MealSlot slot : meals) {
+                if (slot.getType() == type) {
+                    slot.setRecipe(recipe);
+                    return;
+                }
+            }
+            throw new IllegalStateException("No slot of type " + type + " in this DayPlan");
+        } else {
+            //in case of snack need to choose the slot with recipe = null
+            for (MealSlot slot : meals) {
+                if (slot.getType() == MealType.SNACK && slot.getRecipe() == null) {
+                    slot.setRecipe(recipe);
+                    return;
+                }
+            }
+            throw new IllegalStateException("All snack slots already used for day "+day);
+        }
+    }
+
     @Override
     public String toString() {
         return "DayPlan " + day +
